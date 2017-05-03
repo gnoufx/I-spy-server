c94b46d8774870e812a2dbe6229fdc0cfa35fab0
Alexandre <alexdu98@users.noreply.github.com>
2017-05-03 12:03:57 +0200

    Création des tables user et user_phone

--- database/create.sql
+++ database/create.sql
@@ -1,23 +1,100 @@
-/*-----------------------------------------------------------------
-----------------------DATABASE CREATION--------------------------
------------------------------------------------------------------*/
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2+deb8u2
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mer 03 Mai 2017 à 12:02
+-- Version du serveur :  5.5.54-0+deb8u1
+-- Version de PHP :  5.6.30-0+deb8u1
 
-CREATE DATABASE IF NOT EXISTS `i_spy_database`
-DEFAULT CHARACTER SET utf8mb4;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-USE `i_spy_database`;
 
-/*---------------------------------------------------------------
-----------------------TABLES CREATION--------------------------
----------------------------------------------------------------*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `i_spy`
+--
+CREATE DATABASE IF NOT EXISTS `i_spy` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `i_spy`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `phone`
+--
 
 CREATE TABLE IF NOT EXISTS `phone` (
-       `phone_id` INT NOT NULL AUTO_INCREMENT,
-       `login` VARCHAR(25) NOT NULL,
-       `password` VARCHAR(25) NOT NULL,
-       `phone_number` VARCHAR(25),
-       `phone_model` VARCHAR(25),
-       CONSTRAINT phone_pk PRIMARY KEY (`phone_id`),
-       CONSTRAINT is_unique_login UNIQUE (`login`),
-       CONSTRAINT is_unique_password UNIQUE (`password`))
-ENGINE = MyISAM CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
+`phone_id` int(11) NOT NULL,
+  `login` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `phone_number` varchar(25) DEFAULT NULL,
+  `phone_model` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+`user_id` int(11) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_phone`
+--
+
+CREATE TABLE IF NOT EXISTS `user_phone` (
+  `user_id` int(11) NOT NULL,
+  `phone_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `phone`
+--
+ALTER TABLE `phone`
+ ADD PRIMARY KEY (`phone_id`), ADD UNIQUE KEY `is_unique_login` (`login`), ADD UNIQUE KEY `is_unique_password` (`password`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`user_id`), ADD UNIQUE KEY `mail` (`mail`);
+
+--
+-- Index pour la table `user_phone`
+--
+ALTER TABLE `user_phone`
+ ADD PRIMARY KEY (`user_id`,`phone_id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `phone`
+--
+ALTER TABLE `phone`
+MODIFY `phone_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
